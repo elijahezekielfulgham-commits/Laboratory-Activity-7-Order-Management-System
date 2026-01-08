ccb441d8ce63a360cc6cc5c3eb6d13bd3156b22f
ElijahDFulgham <elijahezekiel.fulgham@cvsu.edu.ph>
2026-01-08 08:25:46 +0800

    Update OnlineOrder.java

--- src/com/store/order/OnlineOrder.java
+++ src/com/store/order/OnlineOrder.java
@@ -1,1 +1,19 @@
+package com.store.order;
 
+import com.store.payment.Payable;
+
+public class OnlineOrder extends Order implements Payable {
+
+    public OnlineOrder(int orderId, double amount) {
+        super(orderId, amount);
+    }
+    @Override
+    public void processOrder() {
+        System.out.println("The system has started processing the online order with ID " + getOrderId() + ".");
+    }
+    @Override
+    public void pay() {
+        setStatus(OrderStatus.PAID);
+        System.out.println("The payment for order ID " + getOrderId() + " has been successfully confirmed.");
+    }
+}
